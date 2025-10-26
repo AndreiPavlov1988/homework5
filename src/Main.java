c62d83144705a1745ba3b322a8da5969cec3003b
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-10-26 16:12:31 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         for (int i = 1; i <= 10; i = i + 1){
             System.out.println(i);
         }
+//task 2
+        for (int x = 10; x >= 0; x = x - 1) {
+            System.out.println(x);
+        }
     }
 }
