dec707ce33a362d0099124cc98255332a8d2f7d1
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-10-26 16:50:00 +0300

    task 4

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++){
-            System.out.print(i);
+            System.out.print(i + " ");
         }
         System.out.println();
 //task 2
         for (int x = 10; x >= 0; x -- ) {
-            System.out.print(x);
+            System.out.print(x + " ");
         }
         System.out.println();
-//task3
+//task 3
         for (int y = 0; y <= 17; y = y + 2) {
-            System.out.print(y);
+            System.out.print(y + " ");
+        }
+        System.out.println();
+//task 4
+        for (int z = 10; z >= -10; z--) {
+            System.out.print(z + " ");
         }
         System.out.println();
     }
